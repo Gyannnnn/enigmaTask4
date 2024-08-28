2e790ee52b6d78fe8385e7976d91e4fa01e8aa73
Gyanranjan Patra 🐰 <153155648+Gyannnnn@users.noreply.github.com>
2024-08-28 16:51:15 +0530

    Update Signup.jsx

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -42,7 +42,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:3000/api/auth/signup', {
+      const res = await fetch('https://enigma-task4api.vercel.app/api/auth/signup', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
